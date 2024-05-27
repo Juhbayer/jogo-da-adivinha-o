2d7e179daa29e5db9e645d524900063671526b90
juhbayer <juliabayer546@gmail.com>
2024-05-27 17:24:54 -0300

    fiz o jogo

--- jogodaadivinhacao/src/jogodaadivinhacao/Jogodaadivinhacao.java
+++ jogodaadivinhacao/src/jogodaadivinhacao/Jogodaadivinhacao.java
@@ -5,6 +5,9 @@
  */
 package jogodaadivinhacao;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author aluno.saolucas
@@ -15,7 +18,29 @@ public class Jogodaadivinhacao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Random random = new Random();
+        int numeroSecreto = random.nextInt(10) + 1;
+        System.out.println("Olá, vamos fazer um jogo. Escolha um número de 1 a 10.");
+
+        Scanner scanner = new Scanner(System.in);
+        int palpite = 0;
+        int contador = 0;
+
+        while (palpite != numeroSecreto) {
+            contador++;
+            System.out.print("Seu palpite: ");
+            palpite = scanner.nextInt();
+
+            if (palpite == numeroSecreto) {
+                System.out.println("Pura sorte! Você acertou o número " + numeroSecreto + " em " + contador + " tentativas. Vamos mais uma, dessa vez você não vai acertar!");
+            } else if (palpite < numeroSecreto) {
+                System.out.println("Número muito baixo, tente novamente.");
+            } else {
+                System.out.println("Número muito alto, tente novamente.");
+            }
+        }
+
+        scanner.close();
     }
     
 }
